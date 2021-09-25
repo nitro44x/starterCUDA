1a52fa48d854b60739a5b9435248b4d14d6eded6
Chris Petrovitch <cpetrovitch@gmail.com>
2021-09-25 18:39:40 -0400

    added catch2 tests (that fail to test actions)

--- src/testApp/main.cpp
+++ src/testApp/main.cpp
@@ -10,7 +10,7 @@
 #include <testLib/testLib.hpp>
 
 int main(int argc, char **argv) {
-    auto const value = launch_simple_kernel();
+    auto const value = kernels::launch_simple_kernel();
     std::cout << "Kernel in exe, one = " << value << std::endl;
 
     auto const valueLib = testLib::call_simpleLib();
